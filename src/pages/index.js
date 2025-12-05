d2636c66e5821b74117fcf1f3c14afe291b4fa30
muhammedshamil8 <muhammedshamil008@gmail.com>
2025-12-06 01:10:26 +0530

    #

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
 export { default as Home } from './Home';
 export { default as Register } from './Register';
-export { default as Committee } from './Committee';
 export { default as Login } from './Login';
 export { default as Dashboard } from './Admin';
-export { default as Abstract } from './AbstractForm';
-export { default as FeedbackForm } from './FeedbackForm';
