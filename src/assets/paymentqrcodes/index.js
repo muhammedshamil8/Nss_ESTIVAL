69b3016ec66eacbed9ba816bfec074fc81f4031d
muhammedshamil8 <muhammedshamil008@gmail.com>
2025-12-08 12:56:54 +0530

    #

--- src/assets/paymentqrcodes/index.js
+++ src/assets/paymentqrcodes/index.js
@@ -1,5 +1,5 @@
 import Group_dance_QR from './Dance.jpeg';
-import Best_volunteer_QR from './Best_Volunteer.jpeg';
+import Best_volunteer_QR from './Best_volunteer.jpeg';
 import Fashion_show_QR from './Fashion_show.jpeg';
 import Spot_photography_QR from './Photography.jpeg';
 
